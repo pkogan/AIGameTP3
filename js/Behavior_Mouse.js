300bbb5b67d2e983e5226cb029de931b6f1a1e2f
dmanrique <dmanrique@localhost>
2016-04-20 14:46:57 -0300

    Pez girando

--- js/Behavior_Mouse.js
+++ js/Behavior_Mouse.js
@@ -21,19 +21,26 @@ Behavior_Mouse.prototype.constructor = Behavior_Mouse;
 
 Behavior_Mouse.prototype.update = function () {
         
-         game.physics.arcade.moveToPointer(this.sprite, 300);
+         game.physics.arcade.moveToPointer(this.sprite, 200);
                 //pezBueno.angle += 2;
-                
+                game.input.maxPointers = 1;
                 var posx =this.sprite.body.x;
                 var posy = this.sprite.body.y;
                 //console.log(posx, posy);
                 
                 
+                // Sigue al mouse
                 if (Phaser.Rectangle.contains(this.sprite.body, game.input.x, game.input.y))
                 {
                     this.sprite.body.velocity.setTo(0, 0);
                 }
+                
+                
+                // Rotación siguiendo al mouse
+                var targetAngle = (360 / (2 * Math.PI)) * game.math.angleBetween(this.sprite.body.x, this.sprite.body.y,this.game.input.activePointer.x, this.game.input.activePointer.y) + 90;
+                if(targetAngle < 0){                    
+                    targetAngle += 360;
+                }               
+                this.sprite.angle = targetAngle;             
     
-    
-}
-;
+};
