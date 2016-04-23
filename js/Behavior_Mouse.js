de6289a6d8e60aef33f4ae4af22177dc3100a414
danielaM <daniela.manrique22@hotmail.com>
2016-04-23 14:41:59 -0300

    Commit con seek

--- js/Behavior_Mouse.js
+++ js/Behavior_Mouse.js
@@ -21,7 +21,7 @@ Behavior_Mouse.prototype.constructor = Behavior_Mouse;
 
 Behavior_Mouse.prototype.update = function () {
         
-         game.physics.arcade.moveToPointer(this.sprite, 200);
+         game.physics.arcade.moveToPointer(this.sprite, 100);
                 //pezBueno.angle += 2;
                 game.input.maxPointers = 1;
                 var posx =this.sprite.body.x;
@@ -34,8 +34,7 @@ Behavior_Mouse.prototype.update = function () {
                 {
                     this.sprite.body.velocity.setTo(0, 0);
                 }
-                
-                
+                                
                 // Rotación siguiendo al mouse
                 var targetAngle = (360 / (2 * Math.PI)) * game.math.angleBetween(this.sprite.body.x, this.sprite.body.y,this.game.input.activePointer.x, this.game.input.activePointer.y) + 90;
                 if(targetAngle < 0){                    
--- js/Behavior_Pursuit.js
+++ js/Behavior_Pursuit.js
@@ -9,19 +9,45 @@
  * @param {type} target
  * @returns {Behavior_Derecho}
  */
-function Behavior_Pursuit(game, posx, posy, key, frame, target) {
-    Behavior.call(this, game, posx, posy, key, frame, target);
-        
+
+var targetPred;
+function Behavior_Pursuit(game, posx, posy, key, frame, target, targetI) {
+    Behavior_Seek.call(this, game, posx, posy, key, frame, target);
     
-    
+    targetPred = targetI;
     return this;
 }
-Behavior_Pursuit.prototype = Object.create(Behavior.prototype);//Defino que es sub clase de Sprite.
+Behavior_Pursuit.prototype = Object.create(Behavior_Seek.prototype);//Defino que es sub clase de Sprite.
 Behavior_Pursuit.prototype.constructor = Behavior_Pursuit;
+
+var t=5;
 
 Behavior_Pursuit.prototype.update = function () {
 
+    // Calcular predicción
+    if(t===5){
+        
+       var vector = Phaser.Point.add(this.target.sprite.body.position, ((this.target.sprite.body.velocity*t)));
+       targetPred.sprite.body.velocity = vector;
+       
+       console.log();
+      // console.log(vector + " " + this.target.sprite.body.velocity);
+       
+       this.seek();
+        
+                
+        
+       t=0; 
+    }
+    else {
+        t++;
+    }
+    // Rotación siguiendo al mouse
+    var targetAngle = (360 / (2 * Math.PI)) * game.math.angleBetween(this.sprite.body.x, this.sprite.body.y,this.target.sprite.body.x, this.target.sprite.body.y) + 90;
+    if(targetAngle < 0){                    
+        targetAngle += 360;
+    }    
     
-    
+    this.sprite.angle = targetAngle;
 }
 ;
