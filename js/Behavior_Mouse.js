34a54a8b948b398ce9ab8abd113d129dd888b512
danielaM <daniela.manrique22@hotmail.com>
2016-04-25 14:18:42 -0300

    Commit Circulos

--- js/Behavior_Mouse.js
+++ js/Behavior_Mouse.js
@@ -22,17 +22,17 @@ Behavior_Mouse.prototype.constructor = Behavior_Mouse;
 Behavior_Mouse.prototype.update = function () {
         
                 // Sigue al mouse y rota siempre y cuando este a 15 pixeles(?) de distancia
-                if (game.physics.arcade.distanceToPointer(this.sprite, game.input.activePointer) > 15)
+                if (game.physics.arcade.distanceToPointer(this.sprite, game.input.activePointer) > 5)
                 {
                     // Movimiento
                     game.physics.arcade.moveToPointer(this.sprite, 100);
                     
-                    // Rotación siguiendo al mouse
-                    var targetAngle = (360 / (2 * Math.PI)) * game.math.angleBetween(this.sprite.body.x, this.sprite.body.y,this.game.input.activePointer.x, this.game.input.activePointer.y) + 90;
-                    if(targetAngle < 0){                    
-                        targetAngle += 360;
-                    }               
-                    this.sprite.angle = targetAngle;    
+//                    // Rotación siguiendo al mouse
+//                    var targetAngle = (360 / (2 * Math.PI)) * game.math.angleBetween(this.sprite.body.x, this.sprite.body.y,this.game.input.activePointer.x, this.game.input.activePointer.y) + 90;
+//                    if(targetAngle < 0){                    
+//                        targetAngle += 360;
+//                    }               
+//                    this.sprite.angle = targetAngle;    
                 }
                 else
                 {
--- js/Behavior_Pursuit.js
+++ js/Behavior_Pursuit.js
@@ -11,50 +11,81 @@
  * @returns {Behavior_Pursuit}
  */
 
-//var targetI;
+
 function Behavior_Pursuit(game, posx, posy, key, frame, target, targetP) {
-    Behavior_Seek.call(this, game, posx, posy, key, frame, target); // Para que siga al target correcto deberia mandarse targetI que tiene la predicción.
+    Behavior.call(this, game, posx, posy, key, frame, target); 
     
     // Agrega el otro objetivo.
     this.targetP = targetP;
+    
+    this.vecReference = new Phaser.Point(0, 0);
+    this.max_vel = 150;
+    this.max_force = 10;
     return this;
 }
-Behavior_Pursuit.prototype = Object.create(Behavior_Seek.prototype);//Defino que es sub clase de Sprite.
+Behavior_Pursuit.prototype = Object.create(Behavior.prototype);//Defino que es sub clase de Sprite.
 Behavior_Pursuit.prototype.constructor = Behavior_Pursuit;
 
-var t=5;
+var t=10;
 
 Behavior_Pursuit.prototype.update = function () {
 
     // Calcular predicción
-    if(t===5){
+    if(t===10){
         
-       // Calcula el vector de velocidad futura 
+       // Cambia la velocidad del objetivo a la predicción...  
        this.target.sprite.body.velocity.x=this.target.sprite.body.velocity.x*t;
-       this.target.sprite.body.velocity.y=this.target.sprite.body.velocity.y*t
-       // Cambia la velocidad del objetivo a la predicción... La multiplicación no anda!!
+       this.target.sprite.body.velocity.y=this.target.sprite.body.velocity.y*t;      
        Phaser.Point.add(this.target.sprite.body.position, (this.target.sprite.body.velocity));
        
-       if (game.physics.arcade.distanceToPointer(this.sprite, game.input.activePointer) > 15)
+       // Si está muy cerca, para. Seria incluyendo el arrive?
+       if (game.physics.arcade.distanceToPointer(this.sprite, game.input.activePointer) > 10)
             this.seek();
        else
             this.sprite.body.velocity.set(0);
-                
-        
+       
+       this.seek();
+       
+       
        t=0; 
     }
     else {
+        this.target.sprite.body.position=this.targetP.sprite.body.position; // Vuelve a la posicion del objetivo
+        //this.target.sprite.body.velocity=this.targetP.sprite.body.velocity;
         t++;
     }
-    
-    if (game.physics.arcade.distanceToPointer(this.sprite, game.input.activePointer) > 15){
-        // Rotación siguiendo al mouse
-        var targetAngle = (360 / (2 * Math.PI)) * game.math.angleBetween(this.sprite.body.x, this.sprite.body.y,this.target.sprite.body.x, this.target.sprite.body.y) + 90;
-        if(targetAngle < 0){                    
-            targetAngle += 360;
-        }    
+}
 
-        this.sprite.angle = targetAngle;
+// Seek
+Behavior_Pursuit.prototype.seek = function () {
+
+    //console.log("entro" + this.sprite.position);
+    // VELOCIDAD DESEADA --> normalize(target - position) * max_velocity
+    var velDeseada = Phaser.Point.subtract(this.target.sprite.position, this.sprite.position);
+    
+    // Se normaliza la velocidad deseada
+    velDeseada.normalize();
+    
+    // Multiplica por maxima velocidad.
+    velDeseada.multiply(this.max_vel, this.max_vel);
+    //console.log(velDeseada);
+
+    //steering = desired_velocity - velocity
+    var vecSteering = Phaser.Point.subtract(velDeseada, this.sprite.body.velocity);
+    
+    // Verifico que no supere la fuerza máxima --> steering = truncate (steering, max_force)    
+    if (vecSteering.getMagnitudeSq() > (this.max_force*this.max_force)){ // sin multiplicar no anduvo
+        vecSteering.setMagnitude(this.max_force);
+    }
+    
+    // No tomamos en cuenta la masa. steering = steering / mass
+        
+    // velocity = truncate (velocity + steering , max_speed)
+    this.sprite.body.velocity.add(vecSteering.x, vecSteering.y); // hace la suma: velocity + steering
+    console.log(this.sprite.body.velocity);
+    // luego si, verifica que no supere la velocidad maxima
+    if (this.sprite.body.velocity.getMagnitudeSq() > (this.vel * this.vel)) {
+        this.sprite.body.velocity.setMagnitude(this.vel);
     }
 }
 ;
